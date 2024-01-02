6d0f00c0f3984fdb73767b12553af8160b167f2f
Fauzi <brillianfauzi@gmail.com>
2024-01-02 10:49:27 +0700

    feat:setupProject env,config,mysql

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"library_api/config"
+	"library_api/database"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	cfg := config.InitConfig()
+	if cfg == nil {
+		e.Logger.Fatal("tidak bisa start server kesalahan database")
+	}
+
+	db, err := database.InitMySql(*cfg)
+	if err != nil {
+		e.Logger.Fatal("tidak bisa start bro", err.Error())
+	}
+
+	db.AutoMigrate()
+	e.Logger.Fatal(e.Start(":8000"))
+
+}
