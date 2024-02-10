4dbf47ab8d23f7897e3fb76bcca188d9fb5a4662
Fauzi Brillian <brillianfauzi@gmail.com>
2024-02-10 12:11:38 +0700

    feat: resetPassword unit testing

--- features/user/service/service_test.go
+++ features/user/service/service_test.go
@@ -2,7 +2,6 @@ package service_test
 
 import (
 	"errors"
-	"library_api/config"
 	"library_api/features/user"
 	"library_api/features/user/mocks"
 	"library_api/features/user/service"
@@ -210,9 +209,9 @@ func TestResetPassword(t *testing.T) {
 	t.Run("Success Case", func(t *testing.T) {
 		var userID = uint(1)
 		var rolesUser = "admin"
-		var str, _ = golangjwt.GenerateJWT(userID, rolesUser)
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
 		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
-			return []byte(config.InitConfig().JWT), nil
+			return []byte("rahasiabanget"), nil
 		})
 
 		input := user.User{ID: uint(1), NewPassword: "newpass"}
@@ -227,6 +226,149 @@ func TestResetPassword(t *testing.T) {
 
 		assert.NoError(t, err)
 		assert.Equal(t, user.User{ID: uint(1), Password: "hashednewpass"}, resetPassword)
+
+		repoMock.AssertExpectations(t)
+		enkrip.AssertExpectations(t)
+	})
+
+	t.Run("Failed Case - Login Required", func(t *testing.T) {
+		var userID = uint(2)
+		var rolesUser = "user"
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
+		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+			return nil, nil
+		})
+		input := user.User{ID: uint(2), NewPassword: "newpass"}
+
+		users, err := userService.ResetPassword(token, input)
+
+		assert.Error(t, err)
+		assert.Equal(t, user.User{}, users)
+
+		repoMock.AssertExpectations(t)
+	})
+
+	t.Run("Failed Case - Login Required", func(t *testing.T) {
+		var userID = uint(3)
+		var rolesUser = "user"
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
+		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+			return []byte("rahasiabanget"), nil
+		})
+		input := user.User{ID: uint(1), NewPassword: "newpass"}
+
+		users, err := userService.ResetPassword(token, input)
+
+		assert.Error(t, err)
+		assert.Equal(t, user.User{}, users)
+
+		repoMock.AssertExpectations(t)
+	})
+
+	t.Run("Failed Case - User Not Found", func(t *testing.T) {
+		var userID = uint(1)
+		var rolesUser = "admin"
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
+		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+			return []byte("rahasiabanget"), nil
+		})
+		input := user.User{ID: uint(1), NewPassword: "newpass"}
+
+		repoMock.On("GetUserByID", uint(1)).Return(nil, errors.New("user tidak ditemukan")).Once()
+
+		users, err := userService.ResetPassword(token, input)
+
+		assert.Error(t, err)
+		assert.Equal(t, user.User{}, users)
+
+		repoMock.AssertExpectations(t)
+	})
+
+	t.Run("Failed Case - Wrong Password, Admin Required", func(t *testing.T) {
+		var userID = uint(1)
+		var rolesUser = "user"
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
+		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+			return []byte("rahasiabanget"), nil
+		})
+		input := user.User{ID: uint(1), Password: "wrongpass"}
+		baseUser := user.User{ID: uint(1), Password: "hashpass"}
+
+		repoMock.On("GetUserByID", uint(1)).Return(&baseUser, nil).Once()
+		enkrip.On("Compare", baseUser.Password, input.Password).Return(errors.New("password salah")).Once()
+
+		users, err := userService.ResetPassword(token, input)
+
+		assert.Error(t, err)
+		assert.Equal(t, user.User{}, users)
+
+		repoMock.AssertExpectations(t)
+		enkrip.AssertExpectations(t)
+	})
+
+	t.Run("Failed Case - Empty Password", func(t *testing.T) {
+		var userID = uint(1)
+		var rolesUser = "user"
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
+		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+			return []byte("rahasiabanget"), nil
+		})
+
+		input := user.User{ID: uint(1), Password: "", NewPassword: "wrongpass"}
+		baseUser := user.User{ID: uint(1), Password: ""}
+
+		repoMock.On("GetUserByID", uint(1)).Return(&baseUser, nil).Once()
+
+		users, err := userService.ResetPassword(token, input)
+
+		assert.Error(t, err)
+		assert.Equal(t, user.User{}, users)
+
+		repoMock.AssertExpectations(t)
+
+	})
+
+	t.Run("Failed Case - Empty New Password", func(t *testing.T) {
+		var userID = uint(1)
+		var rolesUser = "user"
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
+		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+			return []byte("rahasiabanget"), nil
+		})
+		input := user.User{ID: uint(1), Password: "hashpass", NewPassword: "testis123"}
+		baseUser := user.User{ID: uint(1), Password: "hashpass", NewPassword: ""}
+
+		repoMock.On("GetUserByID", uint(1)).Return(&baseUser, nil).Once()
+		enkrip.On("Compare", baseUser.Password, input.Password).Return(nil).Once()
+		enkrip.On("HashPassword", input.NewPassword).Return("", errors.New("masukkan password baru dengan benar")).Once()
+		users, err := userService.ResetPassword(token, input)
+
+		assert.Error(t, err)
+		assert.Equal(t, user.User{}, users)
+
+		repoMock.AssertExpectations(t)
+		enkrip.AssertExpectations(t)
+	})
+
+	t.Run("Failed Case - Repository Error", func(t *testing.T) {
+		var userID = uint(1)
+		var rolesUser = "admin"
+		var str, _ = golangjwt.GenerateJWT(userID, rolesUser, "rahasiabanget")
+		var token, _ = jwt.Parse(str, func(t *jwt.Token) (interface{}, error) {
+			return []byte("rahasiabanget"), nil
+		})
+		input := user.User{ID: uint(1), Password: "hashpass", NewPassword: "testis123"}
+		baseUser := user.User{ID: uint(1), Password: "hashpass", NewPassword: ""}
+
+		repoMock.On("GetUserByID", uint(1)).Return(&baseUser, nil).Once()
+		repoMock.On("ResetPassword", input).Return(input, errors.New("kesalahan pada database")).Once()
+
+		enkrip.On("HashPassword", input.NewPassword).Return(input.NewPassword, nil).Once()
+
+		users, err := userService.ResetPassword(token, input)
+
+		assert.Error(t, err)
+		assert.Equal(t, user.User{}, users)
 
 		repoMock.AssertExpectations(t)
 		enkrip.AssertExpectations(t)
